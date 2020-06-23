ba76c3639985ba1f6296bc7a0f57e5235c0c9d2c
Arunkumar <arunkumarms3287@gmail.com>
2020-06-23 18:46:49 +0700

    Update app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,50 +1,7 @@
-import * as React from 'react';
-import { getAnimalImages } from '../service';
-import { removeDuplicate } from '../helpers/removeDuplicates';
-import { InfinitRender } from './infiniteRender';
-import { ProgressiveImageLoad } from './progressiveImageLoad';
-import './appStyle.scss';
-
-const backedReoveDubp = removeDuplicate();
-const loadingImage = 'https://media.giphy.com/media/l0dJ49ChzUuKU8Ampv/giphy.gif';
+import * as React from "react";
+import { InfinitRender } from "./infiniteRender";
+import "./appStyle.scss";
 
 export const App = () => {
-    const [urls, setUrl] = React.useState([]);
-    const [isLoading, setLoading] = React.useState(true);
-    const [root, setRoot] = React.useState(document.querySelector(".container"));
-    const getImages = async () => {
-        setLoading(true);
-        let data = await getAnimalImages(20);
-        data = backedReoveDubp(data);
-        const newSet = urls.concat(data);
-        setUrl(newSet);
-        setLoading(false);
-    }
-
-    React.useEffect(() => {
-        getImages();
-    }, []);
-
-    const renderImageList = () => {
-        return urls.map((image, index) => {
-            return (<ProgressiveImageLoad {...image} key={index} root={root} />);
-        });
-    }
-
-    return (
-        <>
-            <div className='ImageContiner'>
-                <InfinitRender
-                    dataLength={urls.length}
-                    next={getImages}
-                    hasMore={true}
-                >
-                    {!!urls.length && renderImageList()}
-                </InfinitRender>
-            </div>
-            {isLoading && (<div className="LoadingInfo">
-                <div className="lds-dual-ring"></div>
-            </div>)}
-        </>
-    );
+  return <InfinitRender />;
 };
